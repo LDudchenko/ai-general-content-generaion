c1cdc2c9d7946a6e166292879ecc7da52871d415
Solmariana <dudchenkoliudmyla@gmail.com>
2025-10-20 22:17:35 +0300

    Done task #1

--- task/t1/image_analysis.py
+++ task/t1/image_analysis.py
@@ -6,7 +6,7 @@ from task.constants import OPENAI_HOST
 # https://platform.openai.com/docs/guides/images-vision?api-mode=chat#analyze-images
 # https://platform.openai.com/docs/guides/images-vision?api-mode=chat&format=base64-encoded#analyze-images
 
-#TODO:
+# TODO:
 # You need to analyse these 2 images:
 #   - https://a-z-animals.com/media/2019/11/Elephant-male-1024x535.jpg
 #   - in this folder we have 'banner.png', load it as encoded data (see documentation)
@@ -18,4 +18,32 @@ from task.constants import OPENAI_HOST
 # ---
 # In the end load both images (url and base64 encoded 'banner.png'), ask "Generate poem based on images" and se what will happen?
 
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
 
+
+
+url = "/v1/chat/completions"
+image_path = "banner.png"
+base64_image = encode_image(image_path)
+message = {
+    "role": "user",
+    "content": [
+        {"type": "text", "text": "Generate poem based on images"},
+        {
+            "type": "image_url",
+            "image_url": {
+                "url": "https://a-z-animals.com/media/2019/11/Elephant-male-1024x535.jpg",
+            },
+        },
+        {
+            "type": "image_url",
+            "image_url": {
+                "url": f"data:image/jpeg;base64,{base64_image}",
+            },
+        },
+    ],
+}
+client = OpenAIClient(OPENAI_HOST + url)
+client.call(model="gpt-4.1-mini", messages=[message])
