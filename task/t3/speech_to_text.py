ff07a927d9f58e99c4ec8f317255b05662c699a2
Solmariana <dudchenkoliudmyla@gmail.com>
2025-10-21 21:58:09 +0300

    Refactored task #3

--- task/t3/speech_to_text.py
+++ task/t3/speech_to_text.py
@@ -16,19 +16,42 @@ from task.constants import OPENAI_HOST, OPENAI_API_KEY
 #   - Use /v1/audio/transcriptions endpoint
 #   - Use whisper-1 or gpt-4o-transcribe model
 
-url="/v1/audio/transcriptions"
-with open("codeus_audio.mp3", "rb") as f:
-    files = {
-        "file": f,
-    }
-    data = {
-        "model": "gpt-4o-transcribe"
-    }
-    headers = {
-        "Authorization": f"Bearer {OPENAI_API_KEY}"
-    }
 
-    response = requests.post(OPENAI_HOST+url, headers=headers, files=files, data=data)
 
-print(response.json())
+class SpeechToTextOpenAIClient:
 
+    def __init__(self):
+        api_key = OPENAI_API_KEY
+        if not api_key:
+            raise ValueError("API key cannot be null or empty")
+
+        self._api_key = "Bearer " + api_key
+        self._endpoint = OPENAI_HOST + "/v1/audio/transcriptions"
+
+    def call(self, file_path: str, print_response = True, **kwargs):
+        headers = {
+            "Authorization": self._api_key
+        }
+        with open(file_path, "rb") as f:
+            files = {
+                "file": f,
+            }
+            data = {
+                "model": "gpt-4o-transcribe",
+                **kwargs
+            }
+
+            response = requests.post(self._endpoint, headers=headers, files=files, data=data)
+
+            if response.status_code == 200:
+                data = response.json()
+                if print_response:
+                    print(json.dumps(data, indent=2))
+
+                return data["text"]
+
+        raise Exception(f"HTTP {response.status_code}: {response.text}")
+
+client = SpeechToTextOpenAIClient()
+text=client.call("codeus_audio.mp3")
+print(text)
